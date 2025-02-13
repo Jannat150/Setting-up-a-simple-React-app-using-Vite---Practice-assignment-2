a9cc09d235eb6ea8a50425ba7636bb80b32f3d42
Jannat150 <cshsjannat2987@gmail.com>
2025-02-13 10:00:15 +0530

    first commit

---
+++ Deployment1/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>My name is Jannat</h1>
+      <div>Contact: abc123@gmail.com</div>
+      <p>Number: 123456789</p>
+    </div>
+  );
+}
+
+export default Home;
